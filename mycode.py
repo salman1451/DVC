e715a35ffb810873aca4a744902e35f30e2ff6d3
Salman <salmanahmed92000@gmail.com>
2026-01-16 12:41:59 +0500

    third version of data saved

--- mycode.py
+++ mycode.py
@@ -11,6 +11,10 @@ new_roc_loc={'Name': 'salman','Age': 28,'City': 'San Francisco'}
 
 df.loc[len(df.index)] = new_roc_loc
 
+new_roc_loc={'Name': 'hammad','Age': 22,'City': 'abtd'}
+
+df.loc[len(df.index)] = new_roc_loc
+
 data_dir='data'
 os.makedirs(data_dir, exist_ok=True)
 file_path=os.path.join(data_dir, 'sample_data.csv')
